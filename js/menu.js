e9ba9b0fcc6f84b692ab90e4dbdf1188bfcfb85a
Matthew Tse <mtse9779@gmail.com>
2016-02-15 11:32:53 -0500

    cleaned up menu and comments

--- js/menu.js
+++ js/menu.js
@@ -2,3 +2,7 @@ $('#toggle').click(function() {
    $(this).toggleClass('active');
    $('#overlay').toggleClass('open');
   });
+$('#menu').click(function() {
+   $('#toggle').toggleClass('active');
+   $('#overlay').toggleClass('open');
+  });
