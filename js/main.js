ac84a61539334aad1e404f9eace161c9a7e06ae1
Matthew Tse <mtse9779@gmail.com>
2016-02-18 21:27:23 -0500

    Revert "attempt to add animation across pages"
    This reverts commit 39780760205c6350eb20a4e2135b37d2845c3b9a.

--- js/main.js
+++ js/main.js
@@ -4,13 +4,9 @@ $(document).ready(function() {
         anchors: ['firstPage', 'secondPage', '3rdPage', '4thpage', 'lastPage'],
         css3: true
     });
-    // if ( $('li.level1.item101').hasClass('active') ) {
-    //   $('#main').addClass('woodwork');
-    // }
 });
 //----------menu toggle----------
 $(document).on('click', "#menu,#toggle", function () {
     $('#toggle').toggleClass('active');
     $('#overlay').toggleClass('open');
-
 });
