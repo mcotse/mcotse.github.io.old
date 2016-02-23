51045af30f085d14ec58dd001ce497f60f73549c
Matthew Tse <mtse9779@gmail.com>
2016-02-22 20:21:53 -0500

    disable animation on mobile

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,16 +12,13 @@ $(document).on('click', "#menu,#toggle", function () {
     $('#toggle').toggleClass('active');
     $('#overlay').toggleClass('open');
 });
-particlesJS.load('particles-js', 'assets/json/particles.json', function() {
-  console.log('callback - particles.js config loaded');
-});
-$(function() {
-
-    $("h3")
-        .wrapInner("<span>")
-
-    $("h3 br")
-        .before("<span class='spacer'>")
-        .after("<span class='spacer'>");
-
-});
+//disable particle animation on mobile
+var el = document.getElementById('hello');
+  if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+    //do nothing
+  }else
+  {
+    particlesJS.load('particles-js', 'assets/json/particles.json', function() {
+      console.log('callback - particles.js config loaded');
+    });
+  }
