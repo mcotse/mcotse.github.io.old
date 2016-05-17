af4229c0e68732a791b345c72dc8f5d568d740b9
Matthew Tse <mtse9779@gmail.com>
2016-05-16 23:52:32 -0400

    disabled autoscroll

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,14 +13,14 @@ $(document).on('click', "#menu,#toggle", function () {
     $('#overlay').toggleClass('open');
 });
 //disable particle animation on mobile
-var el = document.getElementById('hello');
-  if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
-    //do nothing
-  }else
-  {
-    particlesJS.load('particles-js', 'assets/json/particles.json', function() {
-      console.log('callback - particles.js config loaded');
-    });
-  }
+// var el = document.getElementById('hello');
+//   if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+//     //do nothing
+//   }else
+//   {
+//     particlesJS.load('particles-js', 'assets/json/particles.json', function() {
+//       console.log('callback - particles.js config loaded');
+//     });
+//   }
 // adding span between menu link item letters
 // $('#menu').text(function(){ return $(this).text().replace(/(\w)/g, "<span>$&</span>") })
